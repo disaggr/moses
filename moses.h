74e81cb13fce364de2ed971217d76954864020b8
Felix Eberhardt <felix.eberhardt@hpi.de>
2022-10-24 12:04:20 +0000

    The initial commit.

---
+++ moses.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <pthread.h>
+
+namespace moses {
+
+static pthread_once_t key_once;
+static pthread_key_t pg_stack_key;
+
+class PlaceGuardStack {
+	public:
+		PlaceGuardStack() {
+			(void) pthread_once(&key_once, Initialize());
+			arenas = new std::vector<unsigned>();
+			(void) pthread_setspecific(pg_stack_key, this);
+		}
+
+		void Push(unsigned arena) {
+			arenas->push(arena);
+			size_t size = sizeof(arena);
+			mallctl("thread.arena", NULL, NULL, (void *) &arena, &size);
+		}
+
+		unsigned Pop() {
+			unsigned arena = arenas->back();
+			arenas->pop_back();
+			return arena;
+		}
+	private:
+		Initialize() {
+			(void) pthread_key_create(&pg_stack_key, NULL);
+		}
+		std::vector<unsigned> arenas;
+}
+
+class PlaceGuard {
+	public:
+		PlaceGuard(const unsigned arena) {
+			_pg_stack = pthread_getspecific(pg_stack_key);
+			_pg_stack->push(arena);
+		}
+
+		~PlaceGuard() {
+		       _pg_stack->pop();
+	       }
+	private:
+		PlaceGuardStack *_pg_stack;
+}
+
+class IsolatedPlaceGuard {
+}
+
+class ExtendHooks {
+	public:
+		ExtendHooks(int node) _node(node) { }
+		ExtendHooks(std::string file_backing)
+	private:
+		int _node;
+		std::string _file_backing;
+
+
+}
+
+enum Places {
+	local,
+	remote,
+	disaggregated,
+	dontcare
+}
+
+class Moses {
+	public:
+		static Moses& GetInstance() {
+			if(_instance == NULL) {
+				_instance = new Moses();
+			}
+			return _instance;
+		}
+
+		void Initialize() {
+			
+		}
+
+		unsigned GetArena(Places p) {
+
+		}
+	private:
+		Moses() {}
+		Moses *_instance;
+		std::vector 
+}
+
+}
