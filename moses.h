d3715ebf3f0dd916eff3c2d54d80a9e6ec7f1769
Felix Eberhardt <felix.eberhardt@hpi.de>
2022-10-25 19:13:01 +0000

    LOCO

--- moses.h
+++ moses.h
@@ -51,10 +51,22 @@ class PlaceGuard {
 class IsolatedPlaceGuard {
 }
 
+class ExtentHookBase {
+	public:
+		bool ExtentHookAlloc(extent_hooks_t *extent_hooks, void *new_addr, size_t size, size_t alignment, bool *zero, bool commit, unsigned arena_ind)
+		bool ExtentHookDAlloc(extent_hooks_t *extent_hooks, void *addr, size_t size, bool committed, unsigned arena_ind)
+		bool ExtentHookDestroy(extent_hook_t *extent_hooks, void *addr, size_t size, bool committed, unsigned arena_id)
+		bool ExtentHookCommit(extent_hook_t *extent_hooks, void *addr, size_t size, size_t offset, size_t length, unsigned arena_id)
+		bool ExtentHookDecommit(extent_hook_t *extent_hooks, void *addr, size_t size, size_t offset, size_t length, unsigned arena_id)
+		bool ExtentHookPurge(extent_hook_t *extent_hooks, void *addr, size_t size, size_t offset, size_t length, unsigned arena_id)
+		bool ExtentSplit(extent_hook_t *extent_hooks, void *addr, size_t size, size_t size_a, size_t size_b, bool committed, unsigned arena_id)
+		bool ExtentMerge(extent_hook_t *extent_hooks, void *addr_a, size_t size_a, void *addr_b, size_t size_b, bool committed, unsigned arena_id)
+}
+
 class ExtendHookNuma {
 	public:
 		ExtendHookNuma(int node) : _node(node) { }
-		alloc(
+		bool extent_dalloc_hook(extent_hooks_t *extent_hooks, void *addr, size_t size, bool committed, unsigned arena_ind)
 	private:
 		int _node;
 }
@@ -87,7 +99,11 @@ class Moses {
 		}
 
 		void Initialize() {
-			
+			unsigned arena_ind;
+			size_t unsigned_sz = sizeof(unsigned);
+			extent_hooks_t hooks;
+			size_t hooks_sz = sizeof(extent_hooks_t);
+			mallctl("arenas.create", (void *) &arena_ind, sz, (void *) hooks, hooks_sz);	
 		}
 
 		unsigned GetArena(Places p) {
